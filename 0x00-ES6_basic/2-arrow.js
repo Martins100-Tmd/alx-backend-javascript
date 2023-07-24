ab1121088b575b13f0ab79b609946b2b1f83e7e1
Martins100-Tmd <Olaitanfabayo@gmail.com>
2023-07-24 23:51:31 +0000

    Task 3

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,9 +2,8 @@ export default function getNeighbourhoodsList() {
 	this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
 	const self = this;
-	self.addNeighborhood = (newNeighborhood) => {
+	this.addNeighborhood = (newNeighborhood) => {
 		self.sanFranciscoNeighborhoods.push(newNeighborhood);
-		console.log(self.sanFranciscoNeighborhoods);
 		return self.sanFranciscoNeighborhoods;
 	};
 }
