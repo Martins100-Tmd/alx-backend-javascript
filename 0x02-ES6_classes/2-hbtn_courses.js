d6f62f7d9180c028073a06bb206c0b1657c347cc
Martins100-Tmd <Olaitanfabayo@gmail.com>
2023-07-28 19:11:17 +0000

    classes

---
+++ 0x02-ES6_classes/2-hbtn_courses.js
@@ -0,0 +1,48 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === 'string') this._name = name;
+    else return new TypeError('Name must be a string');
+
+    if (typeof length === 'number') this._length = length;
+    else return new TypeError('Length must be a number');
+
+    this._students = students;
+  }
+
+  set name(str) {
+    if (typeof str === 'string') {
+      this._name = str;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set length(num) {
+    if (typeof num === 'number') {
+      this._length = num;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set students(arr) {
+    if (typeof arr === 'object') {
+      this._students = arr;
+    } else {
+      throw new TypeError('Students must be an array');
+    }
+  }
+
+  get students() {
+    return this._name;
+  }
+}
+
