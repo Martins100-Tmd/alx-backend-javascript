2bbb0cc2f257fd74642cb0b5c161ff66ed40ea0b
Martins100-Tmd <Olaitanfabayo@gmail.com>
2023-07-28 20:26:01 +0000

    classes

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,4 +1,4 @@
-import Building from './5-building';
+import Building from "./5-building";
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
@@ -10,25 +10,11 @@ export default class SkyHighBuilding extends Building {
     return this._floors;
   }
 
-  set floors(floor) {
-    if (typeof floor === 'number') this._floors = floor;
-    throw new TypeError('Floors must be a number');
-  }
-
-  get sqft() {
-    return this._sqft;
-  }
-
-  set sqft(sqft) {
-    if (typeof sqft === 'number') this._sqft = sqft;
-    else throw new TypeError('Sqft must be a number');
+  set floors(floors) {
+    this._floors = floors;
   }
 
   evacuationWarningMessage() {
-    this.evacuationWarningMessage = function eWM() {
-      return `Evacuate slowly the ${this._floors} floors`;
-    };
-    return this.evacuationWarningMessage();
+    return `Evacuate slowly the ${this._floors} floors`
   }
 }
-
