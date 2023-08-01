2bfb5490454b76fce36889c5db28e2a52e7972a3
Martins100-Tmd <Olaitanfabayo@gmail.com>
2023-08-01 20:39:29 +0000

    ES6 Data Manipulation using eslint (airbnb-base specifically) we manipulated datas using various data structures(e.g sets, weakmaps, and maps)  and es6 latest features like export && import statement

--- 0x03-ES6_data_manipulation/commit.sh
+++ 0x03-ES6_data_manipulation/commit.sh
@@ -2,5 +2,3 @@ git add .
 git commit -m 'ES6 Data Manipulation
 using eslint (airbnb-base specifically) we manipulated datas using various data structures(e.g sets, weakmaps, and maps)  and es6 latest features like export && import statement'
 git push -u origin master
-Martins100-Tmd
-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
