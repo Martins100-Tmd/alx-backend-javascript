d970a505a180e7006ba2766559c84c4d5e370bb6
Martins100-Tmd <Olaitanfabayo@gmail.com>
2023-08-01 20:54:46 +0000

    ES6 Data Manipulation using eslint (airbnb-base specifically) we manipulated datas using various data structures(e.g sets, weakmaps, and maps)  and es6 latest features like export && import statement

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,6 @@
 export default function updateUniqueItems(map) {
   const newMap = new Map();
-  if (typeof map !== 'object') throw new Error('Cannot process');
+  if (typeof map !== 'object' || !(map instanceof Map)) throw new Error('Cannot process');
   Array.from(map).forEach((item) => {
     const [k, v] = item;
     if (v === 1) newMap.set(k, 100);
