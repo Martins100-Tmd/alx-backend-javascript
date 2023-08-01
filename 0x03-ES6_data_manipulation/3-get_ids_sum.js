a91478af37df53bc81fe98a139adf69c1e5d3225
Martins100-Tmd <Olaitanfabayo@gmail.com>
2023-08-01 20:27:47 +0000

    ES6 Data Manipulation using eslint (airbnb-base specifically) we manipulated datas using various data structures(e.g sets, weakmaps, and maps)  and es6 latest features like export && import statement

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,5 @@
 export default function getStudentIdsSum(student) {
-  if (typeof student !== "object") return [];
+  if (typeof student !== 'object') return [];
   return student.reduce((prev, curr) => prev + curr.id, 0);
 }
 
